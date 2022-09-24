0529a53d4a574137d1f66e707173dc3e12b7f4dc
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-09-24 12:45:30 -0500

    Update api calls to include other data

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,11 @@
 /* Imports */
 import './css/styles.css'
+import Destination from './destination'
+import { getDestinationData } from './apiCalls'
+import Traveler from './traveler'
+import { getTravelerData } from './apiCalls'
 import Trip from './trip'
-import {getTripData} from './apiCalls'
+import { getTripData } from './apiCalls'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
@@ -9,14 +13,20 @@ import {getTripData} from './apiCalls'
 /* Query Selectors */
 
 /* Instances */
-let tripData
+let destinationData, travelerData, tripData
 
 /* Functions*/
 
 /* apiCalls */
 const loadAPIData = () => {
-    return Promise.all([getTripData()])
+    return Promise.all([getDestinationData(), getTravelerData(), getTripData()])
     .then(responses => {
+        destinationData = responses[0].destinations.map(destination => new Destination(destination))
+        console.log(destinationData)
+
+        travelerData = responses[0].travelers.map(traveler => new Traveler(traveler))
+        console.log(travelerData)
+        
         tripData = responses[0].trips.map(trip => new Trip(trip))
         console.log(tripData)
     })
